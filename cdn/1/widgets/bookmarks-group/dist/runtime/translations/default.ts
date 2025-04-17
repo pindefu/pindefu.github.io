1214f9b759f6427b81eb6c284fba3fe285b78417
Pinde Fu <pfu@esri.com>
2025-04-16 23:02:18 -0700

    test app

---
+++ cdn/1/widgets/bookmarks-group/dist/runtime/translations/default.ts
@@ -0,0 +1,11 @@
+export default {
+  selectAMapWarning: "Please select a map.",
+  toggleLayer: "Toggle Layer",
+  toggleAllLayers: "Toggle Layers",
+  openUrl: "Open URL",
+  openGoogleMaps: "Open Google Maps",
+  powerPopup: "Power Popup",
+  toggleModal: "Toggle Modal",
+  noFeatureWarning: "No feature selected. Click on a feature in the map.",
+  noLayerWarning: "No layer selected. Click on a layer in the map.",
+};
---
+++ cdn/1/widgets/bookmarks-group/dist/setting/translations/default.ts
@@ -0,0 +1,6 @@
+export default {
+  selectMapWidget: "Select Map Widget",
+  lbl_NccBookmarks: "Neighborhood Centers Bookmarks",
+  lbl_AptBookmarks: "Apartment Retail Bookmarks",
+  lbl_RegBookmarks: "Regional Center Bookmarks"
+}
